8a425584364c70dad102361c886e4f44dc7e649a
Atapakalakoteswari <atapakalakoteswari@gmail.com>
2025-06-07 18:20:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList App />
+    </div>
+  );
+}
+
+export default App;
